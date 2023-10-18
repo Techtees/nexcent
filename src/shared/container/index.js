b2e7a59bb3a0ad6d2d7fecb5084eb962a1667aca
taiwoj121 <taiwoj121@gmail.com>
2023-10-18 23:12:50 +0100

    component container style updated

--- src/shared/container/index.js
+++ src/shared/container/index.js
@@ -5,16 +5,13 @@ width:90%;
 max-width: 1440px; 
 margin:0 auto;
 padding:5rems 0;
-display: flex;
-justify-content: space-between;
-align-items: center;
 
 
 `
 export const Section = styled.div`
 padding: 5rem;
 flex-direction: ${(props) => props.flexDirection};
-background: ${(props)=> props.backgroundColor || 'var(--silver)' }
+background: ${(props)=> props.backgroundColor || 'var(--white)' }
 
 `
 
