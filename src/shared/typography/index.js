98d5fc6cf09276e11706c7f3b918b19d6d28e64a
taiwoj121 <taiwoj121@gmail.com>
2023-10-19 23:04:00 +0100

    styling as props

--- src/shared/typography/index.js
+++ src/shared/typography/index.js
@@ -11,14 +11,16 @@ color: var(--gray-dark)
 export const SectionHeading = styled.h2`
 font-size: 2.25rem;
 font-style: normal;
-line-height: 2.25;
-color: var(--grey-dark);
+font-weight: 600;
+line-height: 2.75rem;
+color: var(--gray-dark);
 `
 export const Text = styled.p`
 width: 100%;
 max-width: 601px;
-font-size: 0.979rem;
+font-size: ${(props) => props.fontSize || '0.979rem'};
 font-style: normal;
 line-height: 1.25rem;
-color: var(--gray)
+color: var(--gray);
+text-align: ${(props) => props.textAlign || 'none'}
 `
